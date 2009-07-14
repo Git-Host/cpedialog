7424ea7dfeabddd04eca9b159a4dd4c93777569e
cpedia <cpedia@a1bfd6c9-7850-0410-82cb-e7152bfa5976>
2009-07-14 14:27:40 +0000

    auto resize the images in the blog content.

--- trunk/cpedialog/jscripts/images-auto-resize.js
+++ trunk/cpedialog/jscripts/images-auto-resize.js
@@ -1,4 +1,23 @@
-YAHOO.util.Event.onDOMReady(function() {
+YAHOO.util.Event.onContentReady("blogcontent",function() {
     //get all the images in blogcontent div.
-    var images = YAHOO.util.Dom.getElementsByTagName("img","blogcontent");
+    var img_div = YAHOO.util.Dom.get("blogcontent");
+    var blog_div = YAHOO.util.Dom.get("blogview");
+    var images = blog_div.getElementsByTagName("img");
+    var width_ = YAHOO.util.Dom.getStyle(img_div, 'width');
+    var div_width = width_.substring(0,width_.length-2);
+    for (var i = 0,len = images.length; i < len; i++) {
+        var img = images[i];
+        if (img.width > div_width) {
+            img.width = div_width;
+            //add url for the resized image.
+/*
+            var a_ = document.createElement("a");
+            a_.href = img.src;
+            a_.target = "_blank";
+            a_.appendChild(img.cloneNode(false));
+            img.parentNode.appendChild(a_);
+            a_.parentNode.removeChild(img);
+*/
+        }
+    }
 });
