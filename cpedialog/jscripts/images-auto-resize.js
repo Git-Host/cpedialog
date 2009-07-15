b0c9de197ea6e39b134c496a6effde2865f7bcb5
cpedia <cpedia@a1bfd6c9-7850-0410-82cb-e7152bfa5976>
2009-07-15 01:37:36 +0000

    image auto resize feature bug fix. Now it works on IE and firefox.

--- cpedialog/jscripts/images-auto-resize.js
+++ cpedialog/jscripts/images-auto-resize.js
@@ -1,23 +1,20 @@
 YAHOO.util.Event.onDOMReady(function() {
     //get all the images in blogcontent div.
-    var img_div = YAHOO.util.Dom.get("blogcontent");
-    var blog_div = YAHOO.util.Dom.get("bd");
+    var blog_div = YAHOO.util.Dom.get("yui-main");
     var images = blog_div.getElementsByTagName("img");
-    var width_ = YAHOO.util.Dom.getStyle(img_div, 'width');
-    var div_width = width_.substring(0,width_.length-2);
     for (var i = 0,len = images.length; i < len; i++) {
         var img = images[i];
+        var img_div = YAHOO.util.Dom.getAncestorByTagName(img,"div");
+        var div_width = img_div.offsetWidth;
+        //var div_width = width_.substring(0,width_.length-2);
         if (img.width > div_width) {
-            debugger;
             img.width = div_width;
             //add url for the resized image.
             var a_ = document.createElement("a");
             a_.href = img.src;
             a_.target = "_blank";
-            a_.appendChild(img.cloneNode(false));
-            img.parentNode.appendChild(a_);
-            a_.parentNode.removeChild(img);
-
+            YAHOO.util.Dom.insertAfter(a_,img);
+            a_.appendChild(img);
         }
     }
 });
