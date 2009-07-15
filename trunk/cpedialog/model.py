a5197de71de12b638ede5beebe2c89070e4df3c1
david.blewett <david.blewett@a1bfd6c9-7850-0410-82cb-e7152bfa5976>
2009-07-15 01:29:28 +0000

    Added a first cut at supporting Google Analytics.

--- trunk/cpedialog/model.py
+++ trunk/cpedialog/model.py
@@ -419,6 +419,9 @@ class CPediaLog(SerializableModel):
     delicious_enable = db.BooleanProperty(default = False)
     delicious_username = db.StringProperty(multiline=False, default='cpedia')
 
+    analytics_enable = db.BooleanProperty(default = False)
+    analytics_web_property_id = db.StringProperty(multiline=False,default='')
+
     google_ajax_feed_enable = db.BooleanProperty(default = True)
     google_ajax_feed_key = db.StringProperty(multiline=False,
                                              default='ABQIAAAAOY_c0tDeN-DKUM-NTZldZhQG0TqTy2vJ9mpRzeM1HVuOe9SdDRSieJccw-q7dBZF5aGxGJ-oZDyf5Q'
