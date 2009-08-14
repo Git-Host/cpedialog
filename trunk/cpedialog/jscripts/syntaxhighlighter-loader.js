36ee17b126315deb6d6112ecb5ccbbd72cad8a55
cpedia <cpedia@a1bfd6c9-7850-0410-82cb-e7152bfa5976>
2009-08-14 10:16:46 +0000

    refactor syntachighlighter: load js using YUI get.

--- trunk/cpedialog/jscripts/syntaxhighlighter-loader.js
+++ trunk/cpedialog/jscripts/syntaxhighlighter-loader.js
@@ -7,97 +7,60 @@ Array.prototype.contains = function (element) {
     return false;
 };
 
-if (!window.BrushUtils) var BrushUtils = (function() {
-    var ut = {
-        baseurl:"",
-        resources:[],
-        brushMap : {js:"JScript",jscript:"JScript",javascript:"JScript",
-            bash:"Bash",shell:"Bash",css:"Css",actionscript3:"AS3",as3:"AS3",cpp:"Cpp",c:"Cpp",
-            csharp:"CSharp",groovy:"Groovy",java:"Java",javafx:"JavaFX",jfx:"JavaFX",
-            perl:"Perl",pl:"Perl",php:"Php",text:"Plain",plain:"Plain",py:"Python",python:"Python",
-            ruby:"Ruby",ror:"Ruby",rails:"Ruby",scala:"Scala",sql:"Sql",xml:"Xml",html:"Xml",xhtml:"Xml",xslt:"Xml"
-        },
-        parseBrush :function() {
-            var pres = document.getElementsByTagName("pre");
-            var brushs = new Array();
-            for (var i = 0; i < pres.length; i++) {
-                var pre = pres[i];
-                var className = pre.className;
-                className = className.toLowerCase();
-                var brush = null;
-                if (className.indexOf('brush:') == 0) {
-                    brush = className.substr(className.indexOf(':') + 1);
-                } else if (pre.getAttribute('name') == 'code') {
-                    brush = className;
-                    pre.className = 'brush:' + className;
-                }
-                if (brush == null)
-                    continue;
-                if (!eval("ut.brushMap." + brush)) {
-                    pre.className = 'brush:text';
-                    brush = "text";
-                }
-                if (!brushs.contains(brush)) {
-                    brushs.push(brush);
-                }
+YAHOO.namespace("highlighter");
+YAHOO.highlighter.loader = (function() {
+    var brushMap = {js:"JScript",jscript:"JScript",javascript:"JScript",
+        bash:"Bash",shell:"Bash",css:"Css",actionscript3:"AS3",as3:"AS3",cpp:"Cpp",c:"Cpp",
+        csharp:"CSharp",groovy:"Groovy",java:"Java",javafx:"JavaFX",jfx:"JavaFX",
+        perl:"Perl",pl:"Perl",php:"Php",text:"Plain",plain:"Plain",py:"Python",python:"Python",
+        ruby:"Ruby",ror:"Ruby",rails:"Ruby",scala:"Scala",sql:"Sql",xml:"Xml",html:"Xml",xhtml:"Xml",xslt:"Xml"
+    };
+
+    function parseBrush() {
+        var pres = document.getElementsByTagName("pre");
+        var brushs = new Array();
+        for (var i = 0; i < pres.length; i++) {
+            var pre = pres[i];
+            var className = pre.className;
+            className = className.toLowerCase();
+            var brush = null;
+            if (className.indexOf('brush:') == 0) {
+                brush = className.substr(className.indexOf(':') + 1);
+            } else if (pre.getAttribute('name') == 'code') {
+                brush = className;
+                pre.className = 'brush:' + className;
             }
-            return brushs;
-        },
-        loadLibs : function(_baseurl, _theme) {
+            if (brush == null)
+                continue;
+            if (!eval("brushMap." + brush)) {
+                pre.className = 'brush:text';
+                brush = "text";
+            }
+            if (!brushs.contains(brush)) {
+                brushs.push(brush);
+            }
+        }
+        return brushs;
+    }
+    return {
+        loadLibs : function(baseurl, _theme) {
+            var resources = [];
             var theme = _theme || "Default";
-            var baseurl = _baseurl;
-            ut.baseurl = baseurl;
-            var brushs = ut.parseBrush();
+            var brushs = parseBrush();
             if (brushs.length == 0)
                 return;
-            ut.loadStyle(baseurl + "styles/shCore.css");
-            ut.loadStyle(baseurl + "styles/shTheme" + theme + ".css");
-            ut.resources.push({url:baseurl + 'scripts/shCore.js',type:'js'});
+            YAHOO.util.Get.css([baseurl + "styles/shCore.css", baseurl + "styles/shTheme" + theme + ".css"]);
+            resources.push(baseurl + 'scripts/shCore.js');
             for (var i = 0; i < brushs.length; i++) {
                 var lib = brushs[i];
-                lib = eval("ut.brushMap." + lib);
-                ut.resources.push({url:baseurl + "scripts/shBrush" + lib + ".js",type:'js'});
+                lib = eval("brushMap." + lib);
+                resources.push(baseurl + "scripts/shBrush" + lib + ".js");
             }
-            ut.loadNext();
-        },
-        loadNext : function() {
-            if (ut.resources.length > 0) {
-                var resource = ut.resources.shift();
-                //var tempUt = ut;
-                var callback = ut.resources.length > 0 ? function() {
-                    ut.loadNext();
-                } : function() {
-                    SyntaxHighlighter.config.clipboardSwf = ut.baseurl + 'scripts/clipboard.swf';
-                    SyntaxHighlighter.highlight();
-                };
-                if (resource.type == 'js') {
-                    ut.loadJs(resource.url, callback);
-                } else if (resource.type = 'css') {
-                    ut.loadStyle(resource.url, callback);
-                }
-            }
-        },
-        loadJs : function(src, callback) {
-            var oHead = document.getElementsByTagName('HEAD').item(0);
-            var script = document.createElement("script");
-            script.type = "text/javascript";
-            script.src = src;
-            script.onreadystatechange = function () {
-                if (this.readyState == 'loaded' || this.readyState == 'complete') {
-                    callback();
-                }
+            var callback = function() {
+                SyntaxHighlighter.config.clipboardSwf = baseurl + 'scripts/clipboard.swf';
+                SyntaxHighlighter.highlight();
             };
-            script.onload = callback;
-            oHead.appendChild(script);
-        },
-        loadStyle : function(url) {
-            var oHead = document.getElementsByTagName('HEAD').item(0);
-            var style = document.createElement("link");
-            style.type = "text/css";
-            style.rel = "stylesheet";
-            style.href = url;
-            oHead.appendChild(style);
+            YAHOO.util.Get.script(resources, {onSuccess:callback});
         }
     };
-    return ut;
 })();
